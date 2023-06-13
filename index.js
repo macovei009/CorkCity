6f367f9f3b2dac478127574164b2ff6159a704bd
Stefan Macovei - Error 404 <80716711+macovei009@users.noreply.github.com>
2023-06-14 02:32:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementsByClassName('toggle-button')[0];
+    const menu = document.getElementsByClassName('menu')[0];
+  
+    toggleButton.addEventListener('click', () => {
+      menu.classList.toggle('active');
+    });
+  });
